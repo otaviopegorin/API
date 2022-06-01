83ccec126b08de0ea76b4ba28e8151dc1cad7f9d
Otávio Pegorin <otavio.pegorin@lecom.com.br>
2022-05-31 23:35:29 -0300

    arrumando classe usuario

--- src/main/java/br/com/ApiStage3/model/Usuario.java
+++ src/main/java/br/com/ApiStage3/model/Usuario.java
@@ -43,6 +43,12 @@ public class Usuario {
 		this.recuperarSenha = false;
 	}
 	
+	public Usuario(String nome, String email, String senha) {
+		this.nome = nome;
+		this.email= email;
+		this.senha=senha;
+	}
+	
 	public Boolean getRecuperarSenha() {
 		return recuperarSenha;
 	}
