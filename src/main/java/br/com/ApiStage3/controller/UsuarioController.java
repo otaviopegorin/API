6893e195e7972b6d0a212d69fc264e8e6929c0e3
Otávio Pegorin <otavio.pegorin@lecom.com.br>
2022-07-04 08:14:43 -0300

    teste plt-1549

--- src/main/java/br/com/ApiStage3/controller/UsuarioController.java
+++ src/main/java/br/com/ApiStage3/controller/UsuarioController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ApiStage3.model.Usuario;
@@ -54,5 +55,12 @@ public class UsuarioController {
 	@PostMapping("/cadastroNovaSenha")
 	public boolean cadastroNovaSenha(@RequestBody Usuario usuario){
 		return usuarioService.cadastroNovaSenha(usuario.getEmail(),usuario.getSenha());
+	}
+	
+	@GetMapping("/plt-1549")
+	public String testes(@RequestParam String CodEmpresa,@RequestParam String NomeEmpresa) {
+		System.out.println(CodEmpresa+"/n"+NomeEmpresa);
+		return "CodEmpresa:  "+CodEmpresa+"  Nome empresa: "+NomeEmpresa;
+		
 	}
 }
