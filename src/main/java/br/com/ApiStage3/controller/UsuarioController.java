e9d1cda52f6d0d275817db78f0edc5c681b993b2
Otávio Pegorin <otavio.pegorin@lecom.com.br>
2022-08-01 18:11:04 -0300

    Testando upload de arquivos

--- src/main/java/br/com/ApiStage3/controller/UsuarioController.java
+++ src/main/java/br/com/ApiStage3/controller/UsuarioController.java
@@ -1,6 +1,10 @@
 package br.com.ApiStage3.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
 
 import javax.websocket.server.ServerEndpoint;
 
@@ -24,6 +28,27 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioService usuarioService;
 
+	
+	@GetMapping
+	public void teste() {
+		try {
+			File arquivo = File.createTempFile("Teste", "txt");
+			if(!arquivo.exists()) {
+				System.out.println("Falha ao criar o arquivo");
+			} else {
+				System.out.println("Arquivo existe");
+				Properties properties = new Properties();
+				properties.put("teste", "ok");
+				System.out.println("tentando gravar no arquivo");
+				try(FileOutputStream out = new FileOutputStream(arquivo);){
+					properties.store(out, "Teste");
+					System.out.println("Arquivo salvo");	
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	@GetMapping("/usuarios")
 	public List<UsuarioDTO> getAllUsuarios() {
