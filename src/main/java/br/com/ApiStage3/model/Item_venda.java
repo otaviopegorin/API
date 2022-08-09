232bd76dfc5097abd06c294a528da5b1f5e71e73
Otávio Pegorin <otavio.pegorin@lecom.com.br>
2022-08-09 07:50:34 -0300

    Arrumando algumas coisas

--- src/main/java/br/com/ApiStage3/model/Item_venda.java
+++ src/main/java/br/com/ApiStage3/model/Item_venda.java
@@ -8,7 +8,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
 @Entity(name = "Item_venda")
