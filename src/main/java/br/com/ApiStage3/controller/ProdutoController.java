df1c0e64150cd1761dc9adece837253addc8e006
Otávio Pegorin <otavio.pegorin@lecom.com.br>
2022-09-10 08:18:55 -0300

    teste

--- src/main/java/br/com/ApiStage3/controller/ProdutoController.java
+++ src/main/java/br/com/ApiStage3/controller/ProdutoController.java
@@ -4,20 +4,19 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.com.ApiStage3.model.Produto;
 import br.com.ApiStage3.model.ProdutoDTO;
 import br.com.ApiStage3.service.ProdutoService;
 
-@Controller()
+@RestController()
 public class ProdutoController {
 
 	@Autowired
@@ -48,7 +47,6 @@ public class ProdutoController {
 	}
 	@CrossOrigin
 	@PostMapping("/criarProduto")
-	@ResponseBody
 	public String criaUsuario( @RequestParam String UUID,
 								@RequestParam String nome, 
 								@RequestParam String preco, 
