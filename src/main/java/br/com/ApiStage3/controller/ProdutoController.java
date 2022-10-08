8ee3aeee5303203dc81bdb34b10696bf7f235052
Otávio Pegorin <otavio.pegorin@lecom.com.br>
2022-10-08 08:31:49 -0300

    Adicionando rota de alteração de produto

--- src/main/java/br/com/ApiStage3/controller/ProdutoController.java
+++ src/main/java/br/com/ApiStage3/controller/ProdutoController.java
@@ -73,7 +73,7 @@ public class ProdutoController {
 		p.setDescricao(descricao);
 		p.setQtd_estoque(Integer.valueOf(qtd_estoque));
 		p.setPreco(Integer.valueOf(preco));
-		if(foto != null) {
+		if(foto.getOriginalFilename() != null || foto.getOriginalFilename().equals("") || foto.getOriginalFilename().isEmpty()) {
 			p.setImg_produto("http://projetoscti.com.br/projetoscti02/testesPegorin/" + UUID + foto.getOriginalFilename());
 		}
 		
