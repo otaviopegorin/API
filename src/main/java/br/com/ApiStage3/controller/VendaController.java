b8e1c143023eb8a5d1d384a6fb13e214f4da6b1e
Otávio Pegorin <otavio.pegorin@lecom.com.br>
2022-10-22 08:19:08 -0300

    teste

--- src/main/java/br/com/ApiStage3/controller/VendaController.java
+++ src/main/java/br/com/ApiStage3/controller/VendaController.java
@@ -34,7 +34,7 @@ public class VendaController {
 	
 	@PostMapping(path =  "/venda",consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public int cadastroNovaVenda(@RequestBody List<ProdutoDTO> produtos) {
-		content.forEach(a->{
+		produtos.forEach(a->{
 			System.out.println(a.getNome());
 		});
 		return 0;
