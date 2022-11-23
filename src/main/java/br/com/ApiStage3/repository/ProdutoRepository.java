a0b2728445175f10838159bd59b7f56d58ee048d
Otávio Pegorin <otavio.pegorin@lecom.com.br>
2022-11-23 16:37:48 -0300

    Arrumando a parte de pesquisa por categoria

--- src/main/java/br/com/ApiStage3/repository/ProdutoRepository.java
+++ src/main/java/br/com/ApiStage3/repository/ProdutoRepository.java
@@ -21,7 +21,7 @@ public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 	@Query(value=" SELECT * FROM PRODUTO WHERE lower(nome) like lower(CONCAT('%',:nomeProduto,'%')) and excluido != true", nativeQuery = true)
 	List<Produto> findLikeName(@Param("nomeProduto") String nome);
 	
-	@Query(value="SELECT * FROM PRODUTO WHERE categoria = :categoria and excluido != true ", nativeQuery = true)
+	@Query(value="SELECT * FROM PRODUTO WHERE categoria = :categoria and excluido != true and qtd_estoque != 0", nativeQuery = true)
 	List<Produto> findByCategoria(@Param("categoria")String categoria);
 
 	Produto findByNomeProduto(String nome);
