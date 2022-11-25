823445780f4d6801c57349b105c15faa83e1591d
Otávio Pegorin <otavio.pegorin@lecom.com.br>
2022-11-25 08:27:37 -0300

    Ordenando a rota dos pedidos

--- src/main/java/br/com/ApiStage3/repository/TesteRepository.java
+++ src/main/java/br/com/ApiStage3/repository/TesteRepository.java
@@ -34,7 +34,7 @@ public class TesteRepository {
 				"select u.img_usuario, u.nomeUsuario,u.email, v.data_venda, v.id_venda from Usuario u "
 				+ "inner join u.vendas v "
 				+ " where v.statusVenda like 'EM ANDAMENTO' "
-				+ " order by v.data_venda"
+				+ " order by v.data_venda asc"
 				);
 		
 		List<AuxPedidoDto> resultList = query.getResultList();
